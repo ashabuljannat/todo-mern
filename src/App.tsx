69f70ab0f0f15cccb255833955af689049f51615
ashabul jannat alif <you@example.com>
2023-10-04 23:27:00 +0600

    clean unnesessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import "./App.css";
-// import { useQuery, gql } from "@apollo/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RestApi from "./pages/RestApi";
 import GraphqlClient from "./pages/GraphqlClient";
-
-// const GET_MONGO_ALL_TODO = gql`
-//   query getMongoAllTodo {
-//     getMongoAllTodo {
-//       _id
-//       title
-//       description
-//       createdAt
-//       updatedAt
-//     }
-//   }
-// `;
 
 const router = createBrowserRouter([
   {
@@ -31,12 +18,6 @@ const router = createBrowserRouter([
   },
 ]);
 function App() {
-  // const { loading, error, data } = useQuery(GET_MONGO_ALL_TODO);
-
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error : {error.message}</p>;
-
-  // console.log("app", data.getMongoAllTodo);
   return (
     <>
       <RouterProvider router={router} />
--- src/main.tsx
+++ src/main.tsx
@@ -14,21 +14,6 @@ export const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-// client
-//   .query({
-//     query: gql`
-//       query GetMongoAllTodo {
-//         getMongoAllTodo {
-//           _id
-//           title
-//           description
-//           createdAt
-//           updatedAt
-//         }
-//       }
-//     `,
-//   })
-//   .then((result) => console.log(11,result.data.getMongoAllTodo));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
