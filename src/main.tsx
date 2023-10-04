1ac58c083c131360de3c8b0e0bc2ce56007f4a1d
ashabul jannat alif <you@example.com>
2023-10-04 23:37:37 +0600

    clean unnecessary code

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  gql,
+
 } from "@apollo/client";
 
 export const client = new ApolloClient({
@@ -14,21 +14,6 @@ export const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-// client
-//   .query({
-//     query: gql`
-//       query GetMongoAllTodo {
-//         getMongoAllTodo {
-//           _id
-//           title
-//           description
-//           createdAt
-//           updatedAt
-//         }
-//       }
-//     `,
-//   })
-//   .then((result) => console.log(11,result.data.getMongoAllTodo));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
